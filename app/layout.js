bbf5b9ff2b05ee322bb9d11117c9474807ba65f3
Celebration-cloud <253773781+Celebration-Adesa-HQ@users.noreply.github.com>
2026-06-15 12:04:30 +0100

    feat: implement root layout with providers, analytics, and Meta Pixel and ignore .agent directory

--- app/layout.js
+++ app/layout.js
@@ -1,4 +1,6 @@
+/* eslint-disable @next/next/no-img-element */
 import { Inter, Montserrat } from "next/font/google";
+import Script from "next/script";
 import "./globals.css";
 import { ThemeProvider } from "next-themes";
 import Providers from "./provider";
@@ -6,6 +8,7 @@ import AppShell from "./appshell";
 import { Analytics } from "@vercel/analytics/next";
 import { SpeedInsights } from "@vercel/speed-insights/next";
 import { constructMetadata } from "@/lib/metadata";
+import Image from "next/image";
 
 const inter = Inter({
   subsets: ["latin"],
@@ -33,9 +36,46 @@ export const viewport = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en" className="dark">
+      <head>
+        {/* Meta Pixel */}
+        <Script id="meta-pixel" strategy="afterInteractive">
+          {`
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;
+            n.push=n;
+            n.loaded=!0;
+            n.version='2.0';
+            n.queue=[];
+            t=b.createElement(e);
+            t.async=!0;
+            t.src=v;
+            s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s);
+            }(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+
+            fbq('init', '1005345295218015');
+            fbq('track', 'PageView');
+          `}
+        </Script>
+      </head>
+
       <body
         className={`${inter.variable} ${montserrat.variable} bg-background text-foreground transition-colors`}
       >
+        {/* Meta Pixel NoScript */}
+        <noscript>
+          <Image
+            height="1"
+            width="1"
+            style={{ display: "none" }}
+            src="https://www.facebook.com/tr?id=1005345295218015&ev=PageView&noscript=1"
+            alt="facebook_meta"
+          />
+        </noscript>
+
         <ThemeProvider
           attribute="class"
           enableSystem={false}
@@ -45,8 +85,10 @@ export default function RootLayout({ children }) {
             <AppShell>{children}</AppShell>
           </Providers>
         </ThemeProvider>
+
         <Analytics />
         <SpeedInsights />
+
         <script
           type="application/ld+json"
           dangerouslySetInnerHTML={{
